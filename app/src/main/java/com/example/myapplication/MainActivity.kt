107ad9f31cfe63403d47fc25e6a7beda02253918
daz <darran.kelinske@gmail.com>
2025-02-11 12:06:46 -0600

    make it so you can test urls

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
+    private lateinit var urlInput: EditText
+    private lateinit var loadButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +21,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         webView = findViewById(R.id.webview)
+        urlInput = findViewById(R.id.urlInput)
+        loadButton = findViewById(R.id.loadButton)
+
         webView.webViewClient = WebViewClient() // Set a WebViewClient to handle page navigation
         webView.settings.javaScriptEnabled = true // Enable JavaScript if needed
-        webView.loadUrl("https://www.well.co/samsclub") // Load your desired URL
-//        webView.loadUrl("https://www.google.com") // Load your desired URL
+
+        loadButton.setOnClickListener {
+            val url = urlInput.text.toString()
+            if (url.isNotEmpty()) {
+                webView.loadUrl(url) // Load the URL from the EditText
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
